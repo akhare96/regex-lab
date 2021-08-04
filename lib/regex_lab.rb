8cc59ff9a6a62d22935953956aee315137351752
Ambar Khare <ambar.khare24@gmail.com>
2021-08-04 10:15:30 -0700

    commit tree 812b0158660a1a1e2dd1161a5d8bfe3ba8b70a21

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+    word.match?(/\b[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    words = text.split(" ")
+    words.grep(/^un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match?(/^[A-Z].+[\.?!]$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.match?(/[0-9]/) && phone.count("0-9") == 10
 end
